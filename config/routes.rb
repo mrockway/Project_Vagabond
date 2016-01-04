94c5a52315744bdd6c0146ab8d2fa1e5f12a8d15
vframbach <vframbach@gmail.com>
2016-01-04 15:23:41 -0800

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,13 @@
 Rails.application.routes.draw do
-  
+ 
   root 'welcome#index'
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   resources :users
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  resources :posts
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
+  resources :sessions, only: [:new, :create, :destroy]
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  
 end
