aea7524f5f811e8452a73ab48a487cb357087aa1
mrockway <mrockway@gmail.com>
2016-01-05 21:10:27 -0800

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,15 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-cities = City.create([{ name: 'San Francisco', description: 'Enter Desc'}, 
-											{ name: 'Atlanta', description: 'Enter Desc Here'}, 
-											{ name: 'London', description: 'Enter Desc Here'}, 
-											{ name: 'Gibraltar', description: 'Enter Desc Here'},
+cities = City.create([{ name: 'San Francisco', description: "A crimson bridge, cable cars, a sparkling bay, and streets lined with elegant Victorian homes—San Francisco is undeniably one of the world’s great cities. Located along the Northern California at the state’s distinctive bend in the coast, the region has an alluring magic that stretches beyond the bay to diverse cities with nightlife and trend-setting cuisine."}, 
+											{ name: 'Atlanta', description: "With 5.5 million residents in the metro and outlying areas, the so-called capital of the South continues to experience explosive growth thanks to southbound Yankees and international immigrants alike. It's also booming as a tourist destination. Beyond the big-ticket downtown attractions you'll find a constellation of superlative restaurants and a palpable Hollywood influence.  Without natural boundaries to control development, Atlanta keeps growing. Yet for all this suburbanization, Atlanta is a pretty city covered with trees and elegant homes. Distinct neighborhoods are like friendly small towns stitched together. The economy is robust, the population is young and creative, and racial tensions are minimal in 'the city too busy to hate."}, 
+											{ name: 'London', description: "London is immersed in history, with more than its share of mind-blowing antiquity and historic splendour. London’s buildings are eye-catching milestones in the city’s unique and compelling biography, and a great many of them – the Tower of London, Westminster Abbey, Big Ben – are familiar landmarks. There’s more than enough innovation (the Shard, the London Eye, the planned Garden Bridge) to put a crackle in the air, but it never drowns out London’s well-preserved, centuries-old narrative. Architectural grandeur rises up all around you in the West End, ancient remains dot the City and charming pubs punctuate the banks of the Thames. Take your pick."}, 
+											{ name: 'Gibraltar', description: "Red pillar boxes, fish-and-chip shops and creaky 1970s seaside hotels; Gibraltar – as British writer Laurie Lee once commented – is a piece of Portsmouth sliced off and towed 500 miles south. As with many colonial outposts, ‘the Rock’ overstates its Britishness, a bonus for pub-grub and afternoon-tea lovers, but a confusing double-take for modern Brits who thought their country had moved on since the days of Lord Nelson memorabilia. Poised strategically at the jaws of Europe and Africa, Gibraltar, with its Palladian architecture and camera-hogging Barbary macaques, makes for an interesting break from Cádiz province's white towns and tapas. Playing an admirable supporting role is the swashbuckling local history; lest we forget, the Rock has been British longer than the United States has been American."},
 											{ name: 'Portland', description: 'Enter Desc Here'},
 											{ name: 'Sydney', description: 'Enter Desc Here'}
 										])
+
+users = User.create([{ email: 'eddy@email', first_name: 'Eddy', last_name: 'Chang', current_city: 'San Francisco', password: '123456', image_file_name: image, image_content_type: 'image/jpeg', image_file_size: '2302954', image_updated_at: '2016-01-05 18:25:45.674781' },
+										 { email: 'victoria@email', first_name: 'Victoria', last_name: 'Frambach', current_city: 'San Francisco', password: '123456', image_file_name: image, image_content_type: 'image/jpeg', image_file_size: '2302954', image_updated_at: '2016-01-05 18:25:45.674781' }])
+
+posts = Post.create([{ title: "The City by the Bay", content: "" }])
